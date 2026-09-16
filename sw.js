e5181f9c93e9a6715098c5851b260760ab21ed78
Ivo-BE <ivo.van.ham@gmail.com>
2026-09-16 11:41:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline-cache voor Vertraagd beeld. Versienummer verhogen bij elke nieuwe index.html.
-const CACHE='vertraging-v3';
+const CACHE='vertraging-v5';
 const FILES=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
