61cbd92d23d5afeccf5aca9677cfafde2a7d9997
Ivo-BE <ivo.van.ham@gmail.com>
2026-09-16 08:49:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Offline-cache voor Vertraagd beeld. Versienummer verhogen bij elke nieuwe index.html.
+const CACHE='vertraging-v1';
+const FILES=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.hostname.endsWith('supabase.co'))return;
+  if(u.origin===location.origin){e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))}
+  else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>new Response('',{status:503}))))}
+});
